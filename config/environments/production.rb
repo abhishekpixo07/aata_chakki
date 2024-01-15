3858ab06aaa764bb310e078bd06f643bbfc66e68
abhishekpixo07 <abhishek.k@pixoatic.com>
2024-01-15 14:51:54 +0530

    Add secret_key_base for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  config.secret_key_base = Rails.application.credentials.production[:secret_key_base]
+
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
